c46e0738c319c4045001111830a132f5ec671bc0
Lukas Wallrich <lukas.wallrich@gmail.com>
2021-03-22 21:30:25 +0000

    Fix tests - remove tidy

--- tests/testthat/test-stat_tests.R
+++ tests/testthat/test-stat_tests.R
@@ -10,7 +10,7 @@ mod2 <- lm_std(mpg ~ wt + hp, weights = cyl)
 mod3 <- lm_std(mpg ~ wt + hp, weights = 1:32)
 
 test_that("lm_std works", {
-  expect_equal(round(tidy(mod1)$estimate[2], 3), -0.585)
-  expect_equal(tidy(mod1), tidy(mod2))
-  expect_equal(round(tidy(mod3)$estimate[2], 3), -0.666)
+  expect_equal(round(summary(mod1)$coefficients[2,1], 3), -0.585)
+  expect_equal(summary(mod1)$coefficients, summary(mod2)$coefficients)
+  expect_equal(round(summary(mod3)$coefficients[2,1], 3), -0.666)
 })
