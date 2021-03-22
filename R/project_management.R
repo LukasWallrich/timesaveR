9e88b20d318733b0b34b79ae9bd7c6d39b118418
Lukas Wallrich <lukas.wallrich@gmail.com>
2021-03-22 16:48:56 +0000

    Expand take_note (any object)

--- R/project_management.R
+++ R/project_management.R
@@ -110,10 +110,22 @@ management_functions_file <- ("
   }}
 
 take_note <- function(...) {{
-  x <- list(...) %>% unlist()
-  cat(paste(x, collapse = ' '))
-  notes <<- c(notes, paste(x, collapse = ' '))
-  invisible(x[[1]])
+  x <- list(...) 
+  if (length(x)>1) {{
+    x <- unlist(x)
+    cat(paste(x, collapse = ' '))
+    notes <<- c(notes, paste(x, collapse = ' '))
+  } else {{
+    x <- x[[1]]
+    if ('data.frame' %in% class(x)) {{
+      res <- capture.output(print.data.frame(data.frame(round_df(x, 3)), row.names = FALSE, right = FALSE))
+    }} else {{
+      res <- capture.output(print(x))
+    }}
+    print(res)
+    notes <<- c(notes, paste(res, collapse = '\n'))
+  }}
+  invisible(x)
 }}
 
                               ")
