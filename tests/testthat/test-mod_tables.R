3c6c294a34900d3a875670923e032a01ac96b5d2
Lukas Wallrich <lukas.wallrich@gmail.com>
2021-03-24 09:00:36 +0000

    Test whether any gt fails

--- tests/testthat/test-mod_tables.R
+++ tests/testthat/test-mod_tables.R
@@ -1,22 +1,27 @@
-library(mice)
 
-set.seed(300688)
-imp <- mice(nhanes)
+library(gt)
 
-mod1 <- with(imp, lm(bmi ~ age))
-mod2 <- with(imp, lm_std(bmi ~ age))
+gt::gt(mtcars)
 
-tab <- report_lm_with_std(mod1, mod2)
-
-test_that("mira accepted", {
-  expect_equal(tab$gt_tab$`_data`$Model1[1], "29.78 (2.59)***")
-})
-
-library(MASS)
-pov_att <- polr(poverty ~ religion + age + gender, data = WVS)
-pov_att_std <- polr_std(poverty ~ religion + age + gender, data = WVS)
-tab <- report_polr_with_std(pov_att, pov_att_std, coef_omit = "\\|")
-
-test_that("mira accepted", {
-  expect_equal(tab$gt_tab$`_data`$Model2[2], "1.25 *** [1.19, 1.32]")
-})
+# library(mice)
+# 
+# set.seed(300688)
+# imp <- mice(nhanes)
+# 
+# mod1 <- with(imp, lm(bmi ~ age))
+# mod2 <- with(imp, lm_std(bmi ~ age))
+# 
+# tab <- report_lm_with_std(mod1, mod2)
+# 
+# test_that("mira accepted", {
+#   expect_equal(tab$gt_tab$`_data`$Model1[1], "29.78 (2.59)***")
+# })
+# 
+# library(MASS)
+# pov_att <- polr(poverty ~ religion + age + gender, data = WVS)
+# pov_att_std <- polr_std(poverty ~ religion + age + gender, data = WVS)
+# tab <- report_polr_with_std(pov_att, pov_att_std, coef_omit = "\\|")
+# 
+# test_that("mira accepted", {
+#   expect_equal(tab$gt_tab$`_data`$Model2[2], "1.25 *** [1.19, 1.32]")
+# })
