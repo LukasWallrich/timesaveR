5da4de4b14c13ea46d25573cd6ef35c907d85fad
Lukas Wallrich <lukas.wallrich@gmail.com>
2023-04-14 22:40:51 +0100

    Change pcor to character vector

--- R/pcor.R
+++ R/pcor.R
@@ -4,7 +4,7 @@
 #' out the effect of one or more other variables.
 #' 
 #' @inheritParams cor_matrix 
-#' @param given One or multiple variables in data. It/they will be parceled out from all other variables in data,
+#' @param given A character vector with one or multiple variables in data. It/they will be parceled out from all other variables in data,
 #' before the correlation table is calculated.
 #' @inheritDotParams cor_matrix
 #' @export 
@@ -13,14 +13,12 @@ pcor_matrix <- function(data, given, ...) {
   
   data %<>% dplyr::select_if(is.numeric)
   
-  given_names <- rlang::enexprs(given) %>% purrr::map_chr(rlang::as_name)
-  
-  for (v in setdiff(names(data), given_names)) {
-    formula <- formula(paste(v, "~", paste(given_names, collapse = " + ")))
+  for (v in setdiff(names(data), given)) {
+    formula <- formula(paste(v, "~", paste(given, collapse = " + ")))
     data[[v]] <- residuals(lm(formula, data))
   }
   
-  data %<>% dplyr::select(-dplyr::all_of(given_names))
+  data %<>% dplyr::select(-dplyr::all_of(given))
   
   class(data) <- c("resid_df", class(data))
   
